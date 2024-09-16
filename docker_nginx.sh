4f74fa5685f3435a3666bfcae10797c06f397fb5
Stepan <stapan.dubrov@gmail.com>
2024-09-16 22:16:25 +0300

    port forwarding

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if docker run -v $(pwd)/data:/data/www/ -v $(pwd)/conf:/etc/nginx/ --name nginx --rm -d nginx 2>&1 | grep -c "Error" >/dev/null; then
+if docker run -v $(pwd)/data:/data/www/ -v $(pwd)/conf:/etc/nginx/ --name nginx --rm -d -p 80:80 nginx 2>&1 | grep -c "Error" >/dev/null; then
 	docker stop nginx>/dev/null
 	echo "Nginx stopped"
 else
